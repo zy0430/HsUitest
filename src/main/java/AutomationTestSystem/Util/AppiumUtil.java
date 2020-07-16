f020620c32e0571f3fdcca9511791f75c6c21c29
张渝 <zhangyu@sensecho.com>
2020-07-16 18:35:49 +0800

    3

--- src/main/java/AutomationTestSystem/Util/AppiumUtil.java
+++ src/main/java/AutomationTestSystem/Util/AppiumUtil.java
@@ -3,7 +3,10 @@ package AutomationTestSystem.Util;
 import AutomationTestSystem.Base.TestStep;
 import com.sun.xml.internal.bind.v2.TODO;
 import org.jsoup.helper.StringUtil;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class AppiumUtil {
     public static WebElement getElement(TestStep step) throws Exception{
@@ -20,7 +23,19 @@ public class AppiumUtil {
         }
         String locatorname = loc.substring(0, idx1);
         String locatorvalue = loc.substring(idx1 + 1, idx2);
-        // TODO 1
+        int locindex = Integer.valueOf(loc.substring(idx2+1,loc.length()-1));
+        List<WebElement> e =null;
+        switch (locatorname) {
+            case "resource-id": {
+                e = step.getAdriver().findElements(By.id(locatorvalue));
+                break;
+            }
+            case "text":
+                e = step.getAdriver().findElementsByAndroidUIAutomator("new UiSelector().text(\""+locatorvalue+"\")");
 
+                String A = "213";
+                String B = "" + A + "";
+
+        }
     }
 }
