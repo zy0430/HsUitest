5e0d97c4d2f5c492b74c829274ae9177542ea3a0
LAPTOP-UDSNSP26\章鱼哥 <648883413@qq.com>
2020-08-11 00:01:35 +0800

    遗留一个添加依赖方法后导致include方法未按顺序执行

--- src/test/java/base/base1/Base1.java
+++ src/test/java/base/base1/Base1.java
@@ -3,13 +3,27 @@ package base.base1;
 import org.testng.annotations.Test;
 
 public class Base1 {
+    int a = 0;
+
     @Test(groups = "1")
-    public void test1() {
+    public void test1() throws InterruptedException {
         System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test1");
+        a += 1;
     }
 
-    @Test(groups = "2")
+    @Test(groups = "2", dependsOnMethods = "test1")
     public void test2() {
-        System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test2");
+        if (1 == a) {
+            System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test2");
+            a += 1;
+        }
+    }
+
+    @Test(groups = "3")
+    public void test3() {
+        System.out.println("当前的a为：" + a);
+        if (2 == a) {
+            System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test3");
+        }
     }
 }
