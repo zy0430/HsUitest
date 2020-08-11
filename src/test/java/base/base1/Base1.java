ca833cbcb036d33d13a636a3bae07d154129440f
张渝 <zhangyu@sensecho.com>
2020-08-11 18:22:23 +0800

    自由练习

--- src/test/java/base/base1/Base1.java
+++ src/test/java/base/base1/Base1.java
@@ -1,29 +1,45 @@
 package base.base1;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Base1 {
-    int a = 0;
 
-    @Test(groups = "1")
-    public void test1() throws InterruptedException {
-        System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test1");
-        a += 1;
+//    @BeforeClass
+//    public void beforeTest() {
+//        int a = 100/0;
+//    }
+//
+//    @BeforeMethod
+//    public void beforeMethod() {
+//        System.out.println("调用方法前");
+//    }
+//
+//    @AfterMethod
+//    public void afterMethod() {
+//        System.out.println("调用方法后");
+//    }
+
+    @Test()
+    public String test1() {
+        return "aa";
     }
 
-    @Test(groups = "2", dependsOnMethods = "test1")
+    @Test
     public void test2() {
-        if (1 == a) {
-            System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test2");
-            a += 1;
-        }
+        System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test2");
     }
 
-    @Test(groups = "3")
+    @Test
     public void test3() {
-        System.out.println("当前的a为：" + a);
-        if (2 == a) {
-            System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test3");
-        }
+        System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test3");
+    }
+
+    @Test
+    public void test4() {
+        System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test4");
     }
 }
--- src/test/java/base/base1/Base2.java
+++ src/test/java/base/base1/Base2.java
@@ -5,11 +5,11 @@ import org.testng.annotations.Test;
 public class Base2 {
     @Test(groups = "3")
     public void test3() {
-        System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test3");
+        System.out.println("Base2当前线程：" + Thread.currentThread().getId() + "：this is test3");
     }
 
     @Test(groups = "4")
     public void test4() {
-        System.out.println("当前线程：" + Thread.currentThread().getId() + "：this is test4");
+        System.out.println("Base2当前线程：" + Thread.currentThread().getId() + "：this is test4");
     }
 }
