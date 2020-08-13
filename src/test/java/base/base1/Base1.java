b101b0c26c53fa897a0a944b1dfbbc595bfe602e
张渝 <zhangyu@sensecho.com>
2020-08-13 18:03:21 +0800

    自由练习 :

--- src/test/java/base/base1/Base1.java
+++ src/test/java/base/base1/Base1.java
@@ -6,21 +6,32 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Factory;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Base1 {
-    @Factory(dataProvider = "zz")
-    public Object[] test1(int num) {
-        Object[] objects = new Object[num];
-        for (int i=0; i<num; i++) {
-            Base2 base2 = new Base2(i+"");
-            objects[i] = base2;
-        }
-        return objects;
+    @DataProvider
+    public static Object[][] NoNameMethod(){
+        return new Object[][]{
+                {"DataWithNoName1"},
+                {"DataWithNoName2"},
+                {"DataWithNoName3"}
+        };
     }
-
-    @DataProvider(name = "zz")
-    public Object[][] num() {
-        return new Object[][]{new Object[]{3}};
+    //指定名称1
+    @DataProvider(name="dataprovider1")
+    public static Object[][] dataProvider1(){
+        return new Object[][]{
+                {"dataprovider1-1"},
+                {"dataprovider1-2"}
+        };
+    }
+    //指定名称2
+    @DataProvider(name="dataprovider2")
+    public static Object[][] dataProvider2(){
+        return new Object[][]{
+                {"dataprovider2-1"},
+                {"dataprovider2-2"}
+        };
     }
 }
--- src/test/java/base/base1/Base2.java
+++ src/test/java/base/base1/Base2.java
@@ -1,22 +1,20 @@
 package base.base1;
 
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
 
 public class Base2 {
-    private String str;
-
-    public Base2(String str) {
-        this.str = str;
-    }
-    @Test
-    public void test1() {
-        System.out.println("this is test1：" + str);
+    @Test(dataProvider="NoNameMethod",dataProviderClass=Base1.class)
+    public void doTestNG(String testdatas) {
+        System.out.println("未指定名称，数据源名为方法名NoNameMethod:"+testdatas);
     }
 
-    @Test(dependsOnMethods = "test1")
-    public void test2() {
-        System.out.println("this is test2：" + str);
-    }
+//    @Test(dataProvider="dataprovider1",dataProviderClass=Base1.class)
+//    public void doTestNG1(String testdatas){
+//        System.out.println("指定名称，数据源名称选择了dataprovider1:"+testdatas);
+//    }
+//
+//    @Test(dataProvider="dataprovider2",dataProviderClass=Base1.class)
+//    public void doTestNG2(String testdatas){
+//        System.out.println("指定名称，数据源名称选择了dataprovider2:"+testdatas);
+//    }
 }
